b5b2cb8261e7f5888c52db7932fa16116128fdeb
faisal0833 <faisal0833@gmail.com>
2022-12-17 20:44:28 +0600

    JavaScript Class # Done

---
+++ script.js
@@ -0,0 +1,110 @@
+// 1. Comparison Operatores
+// ==
+// console.log(9 == 9);
+// console.log(9 == "9");
+
+// // ===
+// console.log(9 === "9");
+
+// // !=
+// console.log(9 != 9);
+
+// // !== not equal value or not equal type
+// console.log(9 !== "9");
+
+// // >
+// console.log(9 > 8);
+
+// // <
+// console.log(9 < 7);
+
+// // >=
+// console.log(9 >= 7);
+
+// // <=
+// console.log(9 <= 7);
+
+// ? ternary
+
+// 2. Logical Operatores 
+
+// &&
+// ||
+// !
+
+// Data && Data
+// let x = 6;
+// let y = 2;
+// console.log(x < 9);
+// console.log(y > 3);
+// console.log(x < 9 && y > 3);
+
+// // ||
+// console.log(x < 9 || y > 3);
+
+// // !
+// console.log(x == y);
+// console.log(!(x == y));
+
+// console.log(6 < 9 && 2 > 3);
+
+// 3. operator Precedence
+//BODMAS
+
+// console.log((36 / 6) * 3 + 2 ** 2 - (3 + 5));
+
+// 6 * 3 + 2 ** 2 - 8
+// 6 * 3 + 4 - 8
+// 18 + 4 - 8
+// 22 - 8
+// 14
+
+// // 4. Template Literals (``)
+// const firstName = "Faisal";
+// const lastName = "Kabir";
+// let age = 34;
+// let job = "Web Developre";
+// const country = "Bangladesh";
+
+// // I'm Faisal Kabir, 34 Years Old Web Developer!
+
+// console.log("I'm Faisal Kabir, 34 Years Old Web Developer!");
+
+// console.log("I'm " + firstName + " " + lastName + ", " + age + " Years old " + job + "!");
+
+// console.log(`I'm ${firstName} ${lastName}, ${age} Years Old ${job}!`);
+
+// console.log(`I love ${country}!`);
+
+// console.log(`First line
+// Second line
+// thrid line
+// Fourth line`);
+
+// console.log(`Bangladesh win ${2 + 1} series`);
+
+// 5. If/Else Statements
+// if(){
+
+// } else {
+
+// }
+
+// if (5 > 6) {
+//     console.log("5");
+// } else {
+//     console.log("Something Else!");
+// }
+
+// if (10 < 15) {
+//     console.log("Display 10");
+// } else {
+//     console.log("Something else!");
+// }
+
+// let faizaAge = 5;
+// if(faizaAge >= 5) {
+//     console.log("Faiza can start school"); 
+// } else {
+//     console.log("Faiza can't start school!");
+// }
